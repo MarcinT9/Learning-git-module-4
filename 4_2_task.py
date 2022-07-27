3fbdc03d5f8f76101e1607379ac791b70df1bc51
Marcin Topczewski <marcintopczewski90@gmail.com>
2022-07-27 21:06:56 +0200

    Change if/else

--- 4_2_task.py
+++ 4_2_task.py
@@ -3,10 +3,8 @@ def is_palindrome(word):
     Function is_palindrome based on string arguments passed.
     Checks if the characters in the string are the same from left to right as from right to left.
     """
-    if word.upper()[::-1] == word.upper():
-        return True
-    else:
-        return False
+    word1 = word.upper()
+    return word1[::-1] == word1
 
 print(is_palindrome('kajak'))
 print(is_palindrome('Marcin'))
